531e9f5a7fd011c782d8c8bc01ac1af2911efe30
Azeem Subhani <asubhani@cinnova.com>
2025-02-04 16:23:57 +0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Azeem Subhani on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
